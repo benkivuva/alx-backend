2183248f61362249cdf752e60c37d66b45ff9c67
benkivuva <benkivuvak5@gmail.com>
2023-12-20 23:53:40 -0800

    add:index_range function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function for calculating the start and end indices
+for pagination.
+"""
+
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate the start and end indices for a given page and page size.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices.
+    """
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Page and page_size must be positive integers.")
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
